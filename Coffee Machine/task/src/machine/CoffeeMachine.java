921484e92c3d1f6b1dd9af8106df04a77634d722
Rizal Widyarta Gowandy <rizal.gow@gmail.com>
2020-07-06 19:27:14 +0700

    Finish Stage 2/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,5 +1,7 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
 	private static final String[] steps = {
 		"Starting to make a coffee",
@@ -10,14 +12,29 @@ public class CoffeeMachine {
 		"Pouring some milk into the cup",
 		"Coffee is ready!"
 	};
+	private static final int water = 200;
+	private static final int milk = 50;
+	private static final int bean = 15;
 
 	public static void main(String[] args) {
-		PrintSteps();
+		System.out.println("Write how many cups of coffee you will need:");
+		Scanner in = new Scanner(System.in);
+		int cups = in.nextInt();
+		TotalIngredients(cups);
 	}
 
-	public static void PrintSteps() {
+	public static void Steps() {
 		for (String step : steps) {
 			System.out.println(step);
 		}
+	}
+
+	public static void TotalIngredients(int cups) {
+		int waters = water * cups;
+		int milks = milk * cups;
+		int beans = bean * cups;
+		System.out.println(waters + " ml of water");
+		System.out.println(milks + " ml of milk");
+		System.out.println(beans + " g of coffee beans");
 	}
 }
