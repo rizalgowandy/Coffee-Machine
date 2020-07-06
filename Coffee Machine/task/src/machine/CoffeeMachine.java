770d49d5b89bd746514e2f92ab1e23194e53485c
Rizal Widyarta Gowandy <rizal.gow@gmail.com>
2020-07-06 19:56:47 +0700

    Finish Stage 3/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -3,7 +3,7 @@ package machine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-	private static final String[] steps = {
+	static final String[] steps = {
 		"Starting to make a coffee",
 		"Grinding coffee beans",
 		"Boiling water",
@@ -12,29 +12,64 @@ public class CoffeeMachine {
 		"Pouring some milk into the cup",
 		"Coffee is ready!"
 	};
-	private static final int water = 200;
-	private static final int milk = 50;
-	private static final int bean = 15;
+	static final int requiredWater = 200;
+	static final int requiredMilk = 50;
+	static final int requiredBean = 15;
+	static int waterSupply = 0;
+	static int milkSupply = 0;
+	static int beanSupply = 0;
 
 	public static void main(String[] args) {
-		System.out.println("Write how many cups of coffee you will need:");
-		Scanner in = new Scanner(System.in);
-		int cups = in.nextInt();
-		TotalIngredients(cups);
+		CanMakeOrder();
 	}
 
-	public static void Steps() {
+	static void PrintSteps() {
 		for (String step : steps) {
 			System.out.println(step);
 		}
 	}
 
-	public static void TotalIngredients(int cups) {
-		int waters = water * cups;
-		int milks = milk * cups;
-		int beans = bean * cups;
+	static void PrintTotalIngredients() {
+		System.out.println("Write how many cups of coffee you will need:");
+		Scanner in = new Scanner(System.in);
+		int cups = in.nextInt();
+		int waters = requiredWater * cups;
+		int milks = requiredMilk * cups;
+		int beans = requiredBean * cups;
 		System.out.println(waters + " ml of water");
 		System.out.println(milks + " ml of milk");
 		System.out.println(beans + " g of coffee beans");
+	}
+
+	static void CanMakeOrder() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Write how many ml of water the coffee machine has: ");
+		waterSupply = in.nextInt();
+		System.out.println("Write how many ml of milk the coffee machine has:");
+		milkSupply = in.nextInt();
+		System.out.println("Write how many grams of coffee beans the coffee machine has:");
+		beanSupply = in.nextInt();
+		System.out.println("Write how many cups of coffee you will need:");
+		int cups = in.nextInt();
+		int supply = ToCup(waterSupply, milkSupply, beanSupply);
+		if (cups < supply) {
+			int extra = supply - cups;
+			System.out.printf("Yes, I can make that amount of coffee " +
+				"(and even %d more than that)\n", extra);
+			return;
+		}
+		if (cups == supply) {
+			System.out.println("Yes, I can make that amount of coffee");
+			return;
+		}
+		System.out.printf("No, I can make only %d cup(s) of coffee\n", supply);
+	}
+
+	static int ToCup(int water, int milk, int bean) {
+		int maxWater = water / requiredWater;
+		int maxMilk = milk / requiredMilk;
+		int maxBean = bean / requiredBean;
+		int result = Math.min(maxWater, maxMilk);
+		return Math.min(result, maxBean);
 	}
 }
